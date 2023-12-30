e00a6e7f48c23978c39d8804151f863ec306afb8
xiaoyvxie <153999773+xiaoyvxie@users.noreply.github.com>
2023-12-30 21:46:18 +0800

    Update Digital.h

--- 51库/数码管/Digital.h
+++ 51库/数码管/Digital.h
@@ -6,8 +6,11 @@
 #define Digital_pins P0 // 显示 p*0=A,...p*6=G,p*7=Op
 
 void Digital_reveal(unsigned char Digital_x  ,unsigned char Digital_y);
+
 // void Digital_reveal(bit Digital_polarity, unsigned char Digital_x, unsigned char Digital_y);
+
 void Digital_data_processing(unsigned long Digital_data);
+
 void Digital_dynamic(void);
 
 
